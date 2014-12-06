57aa8b6f4d4a8ca47f3f9e5d1b60f511abf1bd68
Bryan Hughes <bryan@theoreticalideations.com>
2014-12-05 16:38:01 -0800

    Copyright fix

--- src/addon.cc
+++ src/addon.cc
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2013-2014 Bryan Hughes <bryan@theoreticalideations.com> (http://theoreticalideations.com)
+Copyright (c) 2014 Bryan Hughes <bryan@theoreticalideations.com> (http://theoreticalideations.com)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
